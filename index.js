ac5cd16505d45970f1843fbaa30144c11316d85e
pmanlukas <pmanlukas@github.com>
2020-09-04 15:47:37 +0200

    adjusted actions yaml and code

--- index.js
+++ index.js
@@ -5,14 +5,36 @@ const wait = require('./wait');
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const github_token = core.getInput('GitHub_Token');
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const context = github.context;
+    if (context.payload.issue == null) {
+      core.setFailed('No issue found.');
+      return;
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    const issue_number = context.payload.issue.number;
+
+    const octokit = new github.GitHub(github_token);
+    const results = await github.repos.listCollaborators({
+      ...context.repo,
+      affiliation: 'outside'
+    });
+
+    const collaborators = results.data.map(user => `@${user.login}`);
+    console.log(JSON.stringify(collaborators));
+
+    const body = `cc ${collaborators.join(' ')}`;
+
+    const new_comment = await github.issues.createComment({
+      ...context.repo,
+      issue_number: issue_number,
+      body: body
+    });
+
+    const comment = new_comment.data;
+
+    core.setOutput('comment_id', comment.id.toString())
   } catch (error) {
     core.setFailed(error.message);
   }
